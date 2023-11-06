c2621210d920a4f4f3241d9c16e31457c4928481
Dave Brown <davembrown55@googlemail.com>
2023-11-06 15:02:12 +0000

    basic functions: add, subtract, multiply, divide created

---
+++ javaScript.js
@@ -0,0 +1,30 @@
+// add
+// subtract
+// multiply
+// divide
+
+let numberOne;
+let operator;
+let numberTwo;
+let result;
+
+const add =  (numberOne, numberTwo) =>  numberOne + numberTwo;
+const subtract =  (numberOne, numberTwo) =>  numberOne - numberTwo;
+const multiply =  (numberOne, numberTwo) =>  numberOne * numberTwo;
+const divide =  (numberOne, numberTwo) =>  numberOne / numberTwo;
+
+// console.log(add(1,5));
+// console.log(add(10,50));
+// console.log(add(12,8));
+
+// console.log(subtract(1,5));
+// console.log(subtract(10,50));
+// console.log(subtract(12,8));
+
+// console.log(multiply(1,5));
+// console.log(multiply(10,50));
+// console.log(multiply(12,8));
+
+// console.log(divide(1,5));
+// console.log(divide(10,50));
+// console.log(divide(12,8));
