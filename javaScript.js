1f710c028eeb327324c79f1badbe0abf82f81ac7
Dave Brown <davembrown55@googlemail.com>
2023-11-08 18:21:31 +0000

    calculator functions working / display issues taken care of / Decimals work / button animation effect

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,23 @@
 const calcDisplay = document.querySelector('#display');
 
-// const calcDisplay = document.querySelector('#display');
-// const calcDisplay = document.querySelector('#display');
-// const calcDisplay = document.querySelector('#display');
-// const calcDisplay = document.querySelector('#display');
+const rtl = () => {
+    // calcDisplay.style.direction = "rtl";
+    calcDisplay.style.textAlign = "right"
+    calcDisplay.style.float = "right"
+};
 
-// calcDisplay.innerHTML = 'change me';
 
+const ltr = () => {
+    // calcDisplay.style.direction = "ltr";
+    calcDisplay.style.textAlign = "left"
+    calcDisplay.style.float = "left"
+};
+
+rtl();
 let numberOne = "";
-let operator;
-let numberTwo;
-let result;
+let operator = null;
+let numberTwo = "";
+let result = "";
 
 const add =  (numOne, numTwo) =>  numOne + numTwo;
 const subtract =  (numOne, numTwo) =>  numOne - numTwo;
@@ -19,46 +26,85 @@ const divide =  (numOne, numTwo) =>  numOne / numTwo;
 
 const operate = (operator, num1, num2) => operator(num1, num2);
 
-console.log(operate(add, 10, 12));
-console.log(operate(subtract, 10, 12));
-console.log(operate(multiply, 10, 12));
-console.log(operate(divide, 20, 5));
-console.log(operator);
+// const decimal = (num) => num.includes('.');
 
+const updateDisplay = (number) => calcDisplay.innerHTML = `${number}`;
+// {
+//     calcDisplay.innerHTML = `${number}`;
+//     // This ensures that the decimal point stays at the end (visually on the left),
+//     // and any additional numbers go to the right of the decimal point (visually on the right).
+//     // if (number.includes('.')) {
+//     //     calcDisplay.innerHTML = number.split('.').join('') + '.';
+//     // }
+// };
 
 const storeVal = function (val) {
+    rtl();
+
     if (typeof operator === 'undefined' || operator === null) {
-        // console.log(`winnets! ${val}` );
-        // parseInt(val, 10);
-        numberOne += val;
-        calcDisplay.innerHTML = `${numberOne}`;
-    }
-    
+        result = "";
+        if(val === "." && numberOne.includes('.')){
+            return;
+        } else {
+            numberOne += val;
+            updateDisplay(numberOne);            
+        }
+    } else{ 
+        if(val === "." && numberTwo.includes('.')){
+            return;
+        } else {
+            numberTwo += val;
+            updateDisplay(numberTwo);            
+        }
+        
+    }    
 };
 
-const resetDisp = function() {
-    numberOne = "0";
-    numberTwo = "0";
+    
+
+
+const storeOperator = (op) => {
+    rtl();
+    operator = op;
+    if(result !== "") {numberOne = result;}
+};
+
+const resetVars = (val) => {
+    if (val === 'C') {
+        resetDisp();
+    } else if(result.toString().length >= 10 ){
+        ltr(); 
+        calcDisplay.innerHTML = `${result}`;
+    } else {calcDisplay.innerHTML = `${result}`;}
+    numberOne = "";
+    numberTwo = "";
     operator = null;
-    result = "0"
-    calcDisplay.innerHTML = `${numberOne}`;
+}    
+
+const resetDisp = () => calcDisplay.innerHTML = `0`;
+
+const getResult = function () {    
+    if(operator === null && numberOne == "") {resetDisp()};    
+    if(operator === null && numberOne !== "") {calcDisplay.innerHTML = `${numberOne}`};    
+    switch(operator) {
+        case "add":
+            result = add(Number(numberOne), Number(numberTwo));            
+            resetVars();
+            break;
+        case "subtract":
+            result = subtract(Number(numberOne), Number(numberTwo)); 
+            resetVars();           
+            break;
+        case "divide":
+            result = divide(Number(numberOne), Number(numberTwo));        
+            resetVars();  
+            break;
+        case "multiply":
+            result = multiply(Number(numberOne), Number(numberTwo));      
+            resetVars();   
+            break;
+    }
 }
 
 
 
-
-// console.log(add(1,5));
-// console.log(add(10,50));
-// console.log(add(12,8));
-
-// console.log(subtract(1,5));
-// console.log(subtract(10,50));
-// console.log(subtract(12,8));
-
-// console.log(multiply(1,5));
-// console.log(multiply(10,50));
-// console.log(multiply(12,8));
-
-// console.log(divide(1,5));
-// console.log(divide(10,50));
-// console.log(divide(12,8));
