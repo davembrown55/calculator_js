3fd39ee26088a2e86b0acfb6540ab5fc088e91ad
Dave Brown <davembrown55@googlemail.com>
2023-11-09 18:31:48 +0000

    small display added to show workings out

--- javaScript.js
+++ javaScript.js
@@ -1,49 +1,65 @@
-const calcDisplay = document.querySelector('#display');
+const calcDisplay = document.querySelector('#main-display');
+const calcSmallDisplay = document.querySelector('#smaller-display');
 
-const rtl = () => {
-    // calcDisplay.style.direction = "rtl";
+const textRight = () => {
     calcDisplay.style.textAlign = "right"
     calcDisplay.style.float = "right"
 };
 
-
-const ltr = () => {
-    // calcDisplay.style.direction = "ltr";
+const textLeft = () => {
     calcDisplay.style.textAlign = "left"
     calcDisplay.style.float = "left"
 };
 
-rtl();
+const resetDisp = () => {
+    textRight();
+    calcDisplay.innerHTML = `0`;}
+
+const smallFontBlack = () => calcSmallDisplay.style.color = "black";
+const resetSmallDisplay = () => {
+    calcSmallDisplay.style.color = "moccasin";
+    workingOut = "";
+    updateDisplay('0');
+}
+
+textRight();
 let numberOne = "";
 let operator = null;
 let numberTwo = "";
 let result = "";
+let workingOut = "";
 
 const add =  (numOne, numTwo) =>  numOne + numTwo;
 const subtract =  (numOne, numTwo) =>  numOne - numTwo;
 const multiply =  (numOne, numTwo) =>  numOne * numTwo;
 const divide =  (numOne, numTwo) =>  numOne / numTwo;
 
-const operate = (operator, num1, num2) => operator(num1, num2);
+const operate = function (operator, numOne, numTwo) { 
+    switch(operator) {
+        case "+":
+            return  add(Number(numOne), Number(numTwo));       
+        case "-":
+            return subtract(Number(numOne), Number(numTwo)); 
+        case "/":
+            return divide(Number(numOne), Number(numTwo));  
+        case "x":
+            return multiply(Number(numOne), Number(numTwo));     
+    }
+}
 
 // const decimal = (num) => num.includes('.');
 
 const updateDisplay = (number) => calcDisplay.innerHTML = `${number}`;
-// {
-//     calcDisplay.innerHTML = `${number}`;
-//     // This ensures that the decimal point stays at the end (visually on the left),
-//     // and any additional numbers go to the right of the decimal point (visually on the right).
-//     // if (number.includes('.')) {
-//     //     calcDisplay.innerHTML = number.split('.').join('') + '.';
-//     // }
-// };
+const updatesmallDisplay = (workingsOut) => calcSmallDisplay.innerHTML = `${workingsOut}`;
 
 const storeVal = function (val) {
-    rtl();
+    textRight();
 
     if (typeof operator === 'undefined' || operator === null) {
         result = "";
-        if(val === "." && numberOne.includes('.')){
+        resetSmallDisplay();
+
+        if(val === "." && numberOne.includes('.')){ // already decimal? do nothing
             return;
         } else {
             numberOne += val;
@@ -55,55 +71,58 @@ const storeVal = function (val) {
         } else {
             numberTwo += val;
             updateDisplay(numberTwo);            
-        }
-        
+        }        
     }    
 };
 
-    
-
-
 const storeOperator = (op) => {
-    rtl();
+    textRight();
     operator = op;
-    if(result !== "") {numberOne = result;}
+    if(result !== "") {
+        workingOut = `${result} ${operator}`;
+        numberOne = result;
+    } else if (result === ""){
+        workingOut += (`${numberOne} ${operator}` );
+    };    
+    smallFontBlack();
+    updatesmallDisplay(workingOut);
 };
+
+// what happens to small screen when equals is pressed / when C is pressed
 
 const resetVars = (val) => {
     if (val === 'C') {
+
         resetDisp();
-    } else if(result.toString().length >= 10 ){
-        ltr(); 
+        resetSmallDisplay();
+    } else if(val !== 'C' && result.toString().length >= 10 ){
+        textLeft(); 
         calcDisplay.innerHTML = `${result}`;
-    } else {calcDisplay.innerHTML = `${result}`;}
+    } else {
+        calcDisplay.innerHTML = `${result}`;
+    }
     numberOne = "";
     numberTwo = "";
     operator = null;
 }    
 
-const resetDisp = () => calcDisplay.innerHTML = `0`;
+
 
 const getResult = function () {    
-    if(operator === null && numberOne == "") {resetDisp()};    
-    if(operator === null && numberOne !== "") {calcDisplay.innerHTML = `${numberOne}`};    
-    switch(operator) {
-        case "add":
-            result = add(Number(numberOne), Number(numberTwo));            
-            resetVars();
-            break;
-        case "subtract":
-            result = subtract(Number(numberOne), Number(numberTwo)); 
-            resetVars();           
-            break;
-        case "divide":
-            result = divide(Number(numberOne), Number(numberTwo));        
-            resetVars();  
-            break;
-        case "multiply":
-            result = multiply(Number(numberOne), Number(numberTwo));      
-            resetVars();   
-            break;
-    }
+    if(operator === null && numberOne == "") {return;};    // if no values stored: do nothing. 
+    if(operator === null && numberOne !== "") { // if only num 1 entered.    
+        calcDisplay.innerHTML = `${numberOne}`
+        return;
+    };  
+    if (workingOut.includes('=')) { // if result already worked out
+        resetSmallDisplay();
+        resetVars();
+    } else if (!workingOut.includes('=')) { // all the rest
+        result = operate(operator, numberOne, numberTwo);
+        workingOut += (` ${numberTwo} =`);
+        updatesmallDisplay(workingOut);
+        resetVars();
+    }    
 }
 
 
